6ee199e4181e98845cd0d3d0b4974d729ebb5a0c
Charles Jamil <astchars@gmail.com>
2019-12-18 16:11:43 -0300

    Adição de tipo scalar

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-// Definição de tipos que serão tratados. Este é o schema
-// Caso o nome seja diferente de typeDef, é necessário atribuí-la no construtor
+// Tipos de dados suportados pelo GraphQL:
+// Int, Float, String, Boolean e ID
+// Caso seja necessário criar um outro tipo, podemos criar um tipo scalar
 const nomeDiferenteTypeDefs = gql`
+	scalar Date
+
 	# Este será o tipo consulta, ou retorno.
 	type Query {
 		ola: String
-		horaAtual: String
+		horaAtual: Date
 	}
 `;
 
-// Conjunto de funções que resolvem/tratam dados.
-// Caso o nome seja diferente de resolvers, é necessário atribuí-la no construtor
 const nomeDiferenteResolvers = {
 
 	// Aqui temos nossas funções
 	Query: {
 		ola () {
-			return 'String qualquer!'
+			return 'String qualquer!';
 		},
 
 		horaAtual () {
-			return `${ new Date }` 
+			return new Date;
 		}
 	}
-
 };
 
 // Construtor
@@ -36,7 +36,6 @@ const server = new ApolloServer({
 });
 
 // Inicialização do servidor.
-// Neste ponto, ainda não está funcional.
 server.listen().then(
 
 	({ url }) => console.log(`Executando em ${url}`)
